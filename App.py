58f4d4af87227846ac2b91a27fb38f7d35e8b58a
Fredsan27 <70611445+Fredsan27@users.noreply.github.com>
2020-10-21 07:40:06 -0500

    Update App.py

--- App.py
+++ App.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def base():
-    return Response(response=json.dumps({"Status": "UP Test Fredy"}),
+    return Response(response=json.dumps({"Status": "UP Test Fredy S"}),
                     status=200,
                     mimetype='application/json')
   
